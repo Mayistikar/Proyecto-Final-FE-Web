ba6661c7d70271c8ad95384b68c71fe652fc5df4
Mayistikar <andersonrodriguezce@gmail.com>
2025-03-27 18:26:52 -0500

    adding project scaffold

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,15 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+
+@Component({
+  selector: 'app-home',
+  imports: [MatButtonModule, MatCardModule, MatGridListModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HomeComponent {
+
+}
