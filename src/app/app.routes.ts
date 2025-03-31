88ca6cd3967c1db81d272a96961a6e879a0a4398
isinning1 <i.sinning@uniandes.edu.co>
2025-03-31 13:32:18 -0500

    feat: registro de vendedor con formulario, validaciones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ManufacturerComponent } from './manufacturer/manufacturer.component';
+import { SellerRegisterComponent } from './seller/seller-register/seller-register.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'manufacturer', component: ManufacturerComponent }
+  { path: 'manufacturer', component: ManufacturerComponent },
+  { path: 'seller-register', component: SellerRegisterComponent },
 ];
