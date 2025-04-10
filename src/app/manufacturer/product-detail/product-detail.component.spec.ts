22227f813755aa8ee9e9ff3e4f7cabb6468cd95f
isinning1 <i.sinning@uniandes.edu.co>
2025-04-10 14:22:25 -0500

    Fix Testing

--- src/app/manufacturer/product-detail/product-detail.component.spec.ts
+++ src/app/manufacturer/product-detail/product-detail.component.spec.ts
@@ -1,23 +1,51 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
 import { ProductDetailComponent } from './product-detail.component';
+import { TranslateModule, TranslateLoader, TranslateFakeLoader, TranslateService, TranslateStore } from '@ngx-translate/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { ProductService } from '../services/product.service';
+
+// Mock para ActivatedRoute con ID simulado
+const activatedRouteMock = {
+  snapshot: {
+    paramMap: {
+      get: (key: string) => '123'  // Devuelve un ID falso
+    }
+  }
+};
+
+// Mock de ProductService
+class MockProductService {
+  getProductById = () => of({
+    id: '123',
+    name: 'Producto Ficticio',
+    description: 'Este es un producto de prueba'
+  });
+}
 
 describe('ProductDetailComponent', () => {
-  let component: ProductDetailComponent;
-  let fixture: ComponentFixture<ProductDetailComponent>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProductDetailComponent]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(ProductDetailComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+      imports: [
+        ProductDetailComponent,
+        HttpClientTestingModule,
+        TranslateModule.forRoot({
+          loader: { provide: TranslateLoader, useClass: TranslateFakeLoader }
+        })
+      ],
+      providers: [
+        TranslateService,
+        TranslateStore,
+        { provide: ProductService, useClass: MockProductService },
+        { provide: ActivatedRoute, useValue: activatedRouteMock }
+      ]
+    }).compileComponents();
   });
 
   it('should create', () => {
+    const fixture = TestBed.createComponent(ProductDetailComponent);
+    const component = fixture.componentInstance;
     expect(component).toBeTruthy();
   });
 });
