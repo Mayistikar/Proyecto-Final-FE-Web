242621fa3c943fa1f96ab90a5d5accfb4db58c33
isinning1 <i.sinning@uniandes.edu.co>
2025-04-19 14:50:12 -0500

    Se genera models para sales-plan

---
+++ src/app/seller/create-sales-plan/create-sales-plan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-sales-plan',
+  templateUrl: './create-sales-plan.component.html',
+  styleUrls: ['./create-sales-plan.component.css']
+})
+export class CreateSalesPlanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
